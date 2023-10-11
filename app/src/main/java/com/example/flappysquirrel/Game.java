3e5ac2fec4edd967daa11541d0c33ce8b80b8ac2
Karan Gupta <tokarangupta@gmail.com>
2023-10-10 18:44:59 -0700

    added basic game, gameLoop classes plus BASIC game loop logic (blank functions)

--- app/src/main/java/com/example/flappysquirrel/Game.java
+++ app/src/main/java/com/example/flappysquirrel/Game.java
@@ -1,13 +1,19 @@
 package com.example.flappysquirrel;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private GameLoop gameLoop;
+    private Context context;
+
     public Game(Context context) {
         super(context);
 
@@ -15,12 +21,15 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
 
-        GameLoop gameLoop = new GameLoop(this, surfaceHolder); // Initialize GameLoop
+        this.context = context;
+        this.gameLoop = new GameLoop(this, surfaceHolder); // Initialize GameLoop
+
+        setFocusable(true);
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
+        gameLoop.startLoop();
     }
 
     @Override
@@ -31,5 +40,29 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(this.gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(this.context, R.color.magenta);
+        canvas.drawText("UPS: " + averageUPS, 100, 20, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(this.gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(this.context, R.color.magenta);
+        canvas.drawText("FPS: " + averageFPS, 100, 40, paint);
+    }
+
+    public void update() {
     }
 }
--- app/src/main/java/com/example/flappysquirrel/GameLoop.java
+++ app/src/main/java/com/example/flappysquirrel/GameLoop.java
@@ -1,9 +1,47 @@
 package com.example.flappysquirrel;
 
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
-public class GameLoop {
-    public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+public class GameLoop extends Thread {
+    private boolean isRunning = false;
+    private SurfaceHolder surfaceHolder;
+    private Game game;
 
+    public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+        this.game = game;
+    }
+
+    public double getAverageUPS() {
+        return 0;
+    }
+
+    public double getAverageFPS() {
+        return 0;
+    }
+
+    public void startLoop() {
+        this.isRunning = true;
+        start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        // Game Loop
+        Canvas canvas;
+        while(isRunning) {
+
+            // Update and render game
+            try {
+                canvas = this.surfaceHolder.lockCanvas(); // Locks and gets associated Canvas object
+                game.update();
+                game.draw(canvas);
+            } catch (IllegalArgumentException e) { // In case Canvas already locked by another method
+                e.printStackTrace();
+            }
+        }
     }
 }
